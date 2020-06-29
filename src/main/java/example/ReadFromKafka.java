40f407916673ad5cce61419225af649113e38a4d
fafaform <006543@g-able.com>
2020-06-29 16:51:24 +0700

    finished version receiving only decrypted json

--- src/main/java/example/ReadFromKafka.java
+++ src/main/java/example/ReadFromKafka.java
@@ -1,56 +1,41 @@
 package example;
 
-import com.esotericsoftware.minlog.Log;
 import org.apache.flink.api.common.functions.FlatMapFunction;
-import org.apache.flink.api.common.functions.MapFunction;
-import org.apache.flink.api.common.serialization.SimpleStringSchema;
-import org.apache.flink.api.common.state.ValueState;
-import org.apache.flink.api.common.state.ValueStateDescriptor;
-import org.apache.flink.api.java.DataSet;
-import org.apache.flink.api.java.tuple.Tuple;
-import org.apache.flink.api.java.tuple.Tuple1;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple3;
-import org.apache.flink.configuration.Configuration;
-import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.JsonNode;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.node.ObjectNode;
-import org.apache.flink.statefun.sdk.annotations.Persisted;
-import org.apache.flink.statefun.sdk.state.PersistedValue;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSink;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
-import org.apache.flink.streaming.api.functions.KeyedProcessFunction.Context;
-import org.apache.flink.streaming.api.functions.KeyedProcessFunction.OnTimerContext;
-import org.apache.flink.streaming.api.functions.co.CoProcessFunction;
+import org.apache.flink.streaming.api.functions.ProcessFunction;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
 import org.apache.flink.streaming.util.serialization.JSONKeyValueDeserializationSchema;
 import org.apache.flink.util.Collector;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Collection;
 import java.util.Properties;
 
 public class ReadFromKafka {
 
     public static String CARD_NUMBER = "CARD_NUMBER";
     public static String TXN_AMT = "TXN_AMT";
+    //// VARIABLES
+    public static String KAFKA_CONSUMER_TOPIC = "flink-from-kafka";
+    public static String KAFKA_PRODUCER_TOPIC = "flink-to-kafka";
+    //// TEST IN CLUSTER
+    public static String BOOTSTRAP_SERVER = "poc01.kbtg:9092,poc02.kbtg:9092,poc03.kbtg:9092";
+    //// TEST IN MY LOCAL
+//    public static String BOOTSTRAP_SERVER = "localhost:9092";
 
     public static Logger LOG = LoggerFactory.getLogger(ReadFromKafka.class);
 
     public static void main(String[] args) throws Exception{
         Properties properties = new Properties();
-        //// VARIABLES
-        //// TEST IN MY LOCAL
-        String kafka_consumer_topic = "fafaform_kafka";
-        properties.setProperty("bootstrap.servers", "localhost:9092");
-        //// TEST IN CLUSTER
-//        String kafka_consumer_topic = "flink-from-kafka";
-//        properties.setProperty("bootstrap.servers", "poc01.kbtg:9092,poc02.kbtg:9092,poc03.kbtg:9092");
-        ////
-        properties.setProperty("group.id", "flink-gid");
+        properties.setProperty("bootstrap.servers", BOOTSTRAP_SERVER);
+//        properties.setProperty("group.id", "flink-gid");
 
         //// END VARIABLES
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
@@ -66,7 +51,7 @@ public class ReadFromKafka {
 //            }
 //        });
         //// RECEIVE JSON
-        FlinkKafkaConsumer<ObjectNode> JsonSource = new FlinkKafkaConsumer(kafka_consumer_topic, new JSONKeyValueDeserializationSchema(false), properties);
+        FlinkKafkaConsumer<ObjectNode> JsonSource = new FlinkKafkaConsumer(KAFKA_CONSUMER_TOPIC, new JSONKeyValueDeserializationSchema(false), properties);
         DataStream<Tuple2<String,Long>> messageStream = env.addSource(JsonSource).flatMap(new FlatMapFunction<ObjectNode, Tuple2<String,Long>>() {
             @Override
             public void flatMap(ObjectNode s, Collector<Tuple2<String,Long>> collector) throws Exception {
@@ -74,6 +59,9 @@ public class ReadFromKafka {
                 collector.collect(new Tuple2<String, Long>(s.get("value").get(CARD_NUMBER).asText(),s.get("value").get(TXN_AMT).asLong()));
             }
         });
+
+        //// PRODUCT KAFKA
+        FlinkKafkaProducer<String> myProducer = new FlinkKafkaProducer(KAFKA_PRODUCER_TOPIC, new ProducerStringSerializationSchema(KAFKA_PRODUCER_TOPIC), properties, FlinkKafkaProducer.Semantic.EXACTLY_ONCE);
 
         /////////////////// EXAMPLE
 //        messageStream.map(new MapFunction<String, String>() {
@@ -97,8 +85,14 @@ public class ReadFromKafka {
         /////////////////// WORKING
         DataStream<Tuple3<String, Long, Long>> accessCounts = messageStream
                 .keyBy(0).process(new CountWithTimeoutFunction());
-        accessCounts.rebalance().print();
+//        accessCounts.rebalance().print();
         ///////////////////
+        DataStreamSink<String> sendingToKafka = accessCounts.process(new ProcessFunction<Tuple3<String, Long, Long>, String>() {
+            @Override
+            public void processElement(Tuple3<String, Long, Long> stringLongLongTuple3, Context context, Collector<String> collector) throws Exception {
+                collector.collect("{\"CARD_NUMBER\":\"" + stringLongLongTuple3.f0 + "\",\"TOTAL_AMOUNT\":" + stringLongLongTuple3.f1 + ",\"COUNT\":" + stringLongLongTuple3.f2 + "}");
+            }
+        }).addSink(myProducer);
 
         env.execute("Read from kafka");
     }
