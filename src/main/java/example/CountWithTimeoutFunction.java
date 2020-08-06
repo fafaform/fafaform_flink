10cd94b0b630413c3a5d01d36ebeadf658a74773
fafaform <006543@g-able.com>
2020-08-06 23:55:36 +0700

    using timestamp instead of processing time

--- src/main/java/example/CountWithTimeoutFunction.java
+++ src/main/java/example/CountWithTimeoutFunction.java
@@ -3,12 +3,13 @@ package example;
 import org.apache.flink.api.common.state.ValueState;
 import org.apache.flink.api.common.state.ValueStateDescriptor;
 import org.apache.flink.api.java.tuple.Tuple;
-import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.api.java.tuple.Tuple4;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.TimeDomain;
+import org.apache.flink.streaming.api.TimerService;
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
 import org.apache.flink.util.Collector;
+import org.apache.flink.util.OutputTag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,15 +21,46 @@ public class CountWithTimeoutFunction extends KeyedProcessFunction<Tuple, Tuple4
     Logger LOG = LoggerFactory.getLogger(CountWithTimeoutFunction.class);
 
     private ValueState<CountWithTimestamp> state;
+    private ValueState<Long> timeDiff;
+    private static int MINUTE = 1;
 
     @Override
     public void open(Configuration parameters) throws Exception {
         state = getRuntimeContext().getState(new ValueStateDescriptor("countState", CountWithTimestamp.class));
+        timeDiff = getRuntimeContext().getState(new ValueStateDescriptor("timeDiffState", Long.TYPE));
     }
 
     @Override
-    public void processElement(Tuple4<String, Double, Long, Long> value, Context ctx, Collector<Tuple4<String, Double, Long, String>> out) throws Exception {
+    public void processElement(Tuple4<String, Double, Long, Long> value, final Context ctx, Collector<Tuple4<String, Double, Long, String>> out) throws Exception {
 
+        if(ctx.timestamp() > ctx.timerService().currentWatermark() + (60000 * MINUTE) && ctx.timerService().currentWatermark() != 0){
+            onTimer(ctx.timestamp(), new OnTimerContext() {
+                @Override
+                public TimeDomain timeDomain() {
+                    return null;
+                }
+
+                @Override
+                public Tuple getCurrentKey() {
+                    return ctx.getCurrentKey();
+                }
+
+                @Override
+                public Long timestamp() {
+                    return ctx.timestamp();
+                }
+
+                @Override
+                public TimerService timerService() {
+                    return ctx.timerService();
+                }
+
+                @Override
+                public <X> void output(OutputTag<X> outputTag, X x) {
+
+                }
+            }, out);
+        }
         // retrieve the current count
         CountWithTimestamp current = state.value();
         if (current == null) {
@@ -43,11 +75,13 @@ public class CountWithTimeoutFunction extends KeyedProcessFunction<Tuple, Tuple4
 //            current.firstModified = ctx.timestamp();
             ////////////////// TIMER SETTING
             // schedule the next timer 60 seconds from the current processing time
-            ctx.timerService().registerProcessingTimeTimer(ctx.timerService().currentProcessingTime() + 60000);
+            timeDiff.update(ctx.timerService().currentProcessingTime() - ctx.timestamp());
+            ctx.timerService().registerProcessingTimeTimer(ctx.timerService().currentProcessingTime() + (60000 * MINUTE));
             // schedule the next timer 60 seconds from TIMESTAMP
 //            ctx.timerService().registerProcessingTimeTimer(value.f2 + 60000);
         }
-        if(value.f2 >= current.firstModified) {
+        if(value.f2 >= current.firstModified && value.f2 <= current.firstModified + (60000 * MINUTE)) {
+            timeDiff.update(ctx.timerService().currentProcessingTime() - ctx.timestamp());
             // update the state's count
             current.count++;
 
@@ -59,6 +93,7 @@ public class CountWithTimeoutFunction extends KeyedProcessFunction<Tuple, Tuple4
 
         // write the state back
         state.update(current);
+        LOG.info("CURRENT TIMESTAMP: " + ctx.timestamp());
     }
 
     @Override
@@ -66,10 +101,22 @@ public class CountWithTimeoutFunction extends KeyedProcessFunction<Tuple, Tuple4
 
         // get the state for the key that scheduled the timer
         CountWithTimestamp result = state.value();
-
+        long timeDif = 0;
+        try {
+            timeDif = timeDiff.value();
+        }catch (NullPointerException ne){
+            LOG.info("TIMEDIFF VALUE: " + timeDiff.value());
+        }
+        if (result != null) {
+            if ((timestamp - timeDif) >= (result.firstModified + (60000 * MINUTE)) || (timestamp - timeDif) > ctx.timerService().currentProcessingTime()) {
 //        LOG.info("==================== TIMEOUT: " + result.key);
-        // emit the state on timeout
-        out.collect(new Tuple4<String, Double, Long, String>(result.key, result.txn_amt, result.count, result.firstModified+"_"+(result.firstModified+60000)));
-        state.clear();
+                // emit the state on timeout
+                out.collect(new Tuple4<String, Double, Long, String>(result.key, result.txn_amt, result.count, result.firstModified + "_" + (result.firstModified + (60000 * MINUTE))));
+                state.clear();
+                timeDiff.clear();
+            }else {
+                ctx.timerService().registerProcessingTimeTimer(ctx.timerService().currentProcessingTime() + (Math.abs((timestamp - timeDif) - (result.firstModified + (60000 * MINUTE)))));
+            }
+        }
     }
 }
--- src/main/java/example/ReadFromKafka.java
+++ src/main/java/example/ReadFromKafka.java
@@ -2,17 +2,16 @@ package example;
 
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.FlatMapFunction;
-import org.apache.flink.api.common.serialization.AbstractDeserializationSchema;
-import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.api.java.tuple.Tuple4;
-import org.apache.flink.api.java.tuple.Tuple6;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSink;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.AssignerWithPeriodicWatermarks;
 import org.apache.flink.streaming.api.functions.ProcessFunction;
+import org.apache.flink.streaming.api.watermark.Watermark;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
 import org.apache.flink.streaming.util.serialization.JSONKeyValueDeserializationSchema;
@@ -20,9 +19,7 @@ import org.apache.flink.util.Collector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
+import javax.annotation.Nullable;
 import java.util.Properties;
 
 public class ReadFromKafka {
@@ -55,8 +52,6 @@ public class ReadFromKafka {
 
         ////////////////////////////////////////////////////////////////
         //// RECEIVE JSON
-//        final SimpleDateFormat receiveFormat = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss.SSS");
-
         FlinkKafkaConsumer<ObjectNode> JsonSource = new FlinkKafkaConsumer(KAFKA_CONSUMER_TOPIC, new JSONKeyValueDeserializationSchema(false), properties);
         DataStream<Tuple3<String,Double,Long>> messageStream = env.addSource(JsonSource).flatMap(new FlatMapFunction<ObjectNode, Tuple3<String,Double,Long>>() {
             @Override
@@ -64,11 +59,25 @@ public class ReadFromKafka {
                 collector.collect(new Tuple3<String, Double, Long>(
                         s.get("value").get(CARD_NUMBER).asText(),
                         s.get("value").get(TXN_AMT).asDouble(),
-//                        receiveFormat.parse(s.get("value").get(TIMESTAMP).asText()).getTime()
                         s.get("value").get(TIMESTAMP).asLong()
                 ));
             }
-        });
+        }).assignTimestampsAndWatermarks(new AssignerWithPeriodicWatermarks<Tuple3<String, Double, Long>>() {
+                    private long MAX_TIMESTAMP;
+                    @Nullable
+                    @Override
+                    public Watermark getCurrentWatermark() {
+                        Watermark watermark = new Watermark(MAX_TIMESTAMP);
+                        return watermark;
+                    }
+
+                    @Override
+                    public long extractTimestamp(Tuple3<String, Double, Long> currentElement, long l) {
+                        long currentWatermark = currentElement.f2;
+                        MAX_TIMESTAMP = Math.max(currentWatermark, MAX_TIMESTAMP);
+                        return currentElement.f2;
+                    }
+                }).rebalance();
 
         ////////////////////////////////////////////////////////////////
         //// RECEIVE RAW
@@ -111,12 +120,12 @@ public class ReadFromKafka {
                                 input.f0, input.f1, input.f2, context.timestamp()));
                     }
                 })
-                .filter(new FilterFunction<Tuple4<String, Double, Long, Long>>() {
-                    @Override
-                    public boolean filter(Tuple4<String, Double, Long, Long> transactionData) throws Exception {
-                        return (transactionData.f2 > (transactionData.f3-60000));
-                    }
-                })
+//                .filter(new FilterFunction<Tuple4<String, Double, Long, Long>>() {
+//                    @Override
+//                    public boolean filter(Tuple4<String, Double, Long, Long> transactionData) throws Exception {
+//                        return (transactionData.f2 > (transactionData.f3 - 60000));
+//                    }
+//                })
                 .keyBy(0).process(new CountWithTimeoutFunction());
 
         DataStreamSink<String> sendingToKafka = accessCounts.process(new ProcessFunction<Tuple4<String, Double, Long, String>, String>() {
